b993013c16f9d934fd0c1248dac032216ae8934a
Zubair Ahmed <zahmed0808@gmail.com>
2026-01-30 12:33:55 -0800

    make all effect types work with positional modulation

--- src/laser_show/animation/effects/shape.clj
+++ src/laser_show/animation/effects/shape.clj
@@ -30,15 +30,17 @@
 
 
 (defn- scale-xf [time-ms bpm params ctx]
-  (let [resolved (effects/resolve-params-global params time-ms bpm ctx)
-        x-scale (double (:x-scale resolved))
-        y-scale (double (:y-scale resolved))]
-    (map (fn [pt]
-           (let [px (double (pt t/X))
-                 py (double (pt t/Y))]
-             (t/update-point-xy pt
-               (* px x-scale)
-               (* py y-scale)))))))
+  (let [get-x-scale (effects/make-param-resolver :x-scale params time-ms bpm ctx)
+        get-y-scale (effects/make-param-resolver :y-scale params time-ms bpm ctx)]
+    (map-indexed
+     (fn [idx pt]
+       (let [px (double (pt t/X))
+             py (double (pt t/Y))
+             x-scale (double (get-x-scale px py idx))
+             y-scale (double (get-y-scale px py idx))]
+         (t/update-point-xy pt
+           (* px x-scale)
+           (* py y-scale)))))))
 
 (effects/register-effect!
  {:id :scale
@@ -71,15 +73,17 @@
 
 
 (defn- translate-xf [time-ms bpm params ctx]
-  (let [resolved (effects/resolve-params-global params time-ms bpm ctx)
-        dx (double (:x resolved))
-        dy (double (:y resolved))]
-    (map (fn [pt]
-           (let [px (double (pt t/X))
-                 py (double (pt t/Y))]
-             (t/update-point-xy pt
-               (+ px dx)
-               (+ py dy)))))))
+  (let [get-x (effects/make-param-resolver :x params time-ms bpm ctx)
+        get-y (effects/make-param-resolver :y params time-ms bpm ctx)]
+    (map-indexed
+     (fn [idx pt]
+       (let [px (double (pt t/X))
+             py (double (pt t/Y))
+             dx (double (get-x px py idx))
+             dy (double (get-y px py idx))]
+         (t/update-point-xy pt
+           (+ px dx)
+           (+ py dy)))))))
 
 (effects/register-effect!
  {:id :translate
@@ -108,17 +112,18 @@
 
 
 (defn- rotation-xf [time-ms bpm params ctx]
-  (let [resolved (effects/resolve-params-global params time-ms bpm ctx)
-        angle (double (:angle resolved))
-        radians (Math/toRadians angle)
-        cos-a (Math/cos radians)
-        sin-a (Math/sin radians)]
-    (map (fn [pt]
-           (let [x (double (pt t/X))
-                 y (double (pt t/Y))]
-             (t/update-point-xy pt
-               (- (* x cos-a) (* y sin-a))
-               (+ (* x sin-a) (* y cos-a))))))))
+  (let [get-angle (effects/make-param-resolver :angle params time-ms bpm ctx)]
+    (map-indexed
+     (fn [idx pt]
+       (let [x (double (pt t/X))
+             y (double (pt t/Y))
+             angle (double (get-angle x y idx))
+             radians (Math/toRadians angle)
+             cos-a (Math/cos radians)
+             sin-a (Math/sin radians)]
+         (t/update-point-xy pt
+           (- (* x cos-a) (* y sin-a))
+           (+ (* x sin-a) (* y cos-a))))))))
 
 (effects/register-effect!
  {:id :rotation
@@ -142,16 +147,17 @@
 
 
 (defn- pinch-bulge-xf [time-ms bpm params ctx]
-  (let [resolved (effects/resolve-params-global params time-ms bpm ctx)
-        amount (double (:amount resolved))]
-    (map (fn [pt]
-           (let [x (double (pt t/X))
-                 y (double (pt t/Y))
-                 distance (Math/sqrt (+ (* x x) (* y y)))
-                 factor (if (zero? distance)
-                          1.0
-                          (Math/pow distance amount))]
-             (t/update-point-xy pt (* x factor) (* y factor)))))))
+  (let [get-amount (effects/make-param-resolver :amount params time-ms bpm ctx)]
+    (map-indexed
+     (fn [idx pt]
+       (let [x (double (pt t/X))
+             y (double (pt t/Y))
+             amount (double (get-amount x y idx))
+             distance (Math/sqrt (+ (* x x) (* y y)))
+             factor (if (zero? distance)
+                      1.0
+                      (Math/pow distance amount))]
+         (t/update-point-xy pt (* x factor) (* y factor)))))))
 
 (effects/register-effect!
  {:id :pinch-bulge
@@ -180,30 +186,38 @@
    - bl (bottom-left): maps from (-1, -1)
    - br (bottom-right): maps from (1, -1)"
   [time-ms bpm params ctx]
-  (let [resolved (effects/resolve-params-global params time-ms bpm ctx)
-        tl-x (double (:tl-x resolved)) tl-y (double (:tl-y resolved))
-        tr-x (double (:tr-x resolved)) tr-y (double (:tr-y resolved))
-        bl-x (double (:bl-x resolved)) bl-y (double (:bl-y resolved))
-        br-x (double (:br-x resolved)) br-y (double (:br-y resolved))]
-    (map (fn [pt]
-           (let [x (double (pt t/X))
-                 y (double (pt t/Y))
-                 ;; Convert from [-1,1] to [0,1] for interpolation
-                 u (/ (+ x 1.0) 2.0)  ; 0 at left, 1 at right
-                 v (/ (+ y 1.0) 2.0)  ; 0 at bottom, 1 at top
-                 ;; Bilinear interpolation
-                 ;; P = (1-u)(1-v)*BL + u*(1-v)*BR + (1-u)*v*TL + u*v*TR
-                 one-minus-u (- 1.0 u)
-                 one-minus-v (- 1.0 v)
-                 new-x (+ (* one-minus-u one-minus-v bl-x)
-                          (* u one-minus-v br-x)
-                          (* one-minus-u v tl-x)
-                          (* u v tr-x))
-                 new-y (+ (* one-minus-u one-minus-v bl-y)
-                          (* u one-minus-v br-y)
-                          (* one-minus-u v tl-y)
-                          (* u v tr-y))]
-             (t/update-point-xy pt new-x new-y))))))
+  (let [get-tl-x (effects/make-param-resolver :tl-x params time-ms bpm ctx)
+        get-tl-y (effects/make-param-resolver :tl-y params time-ms bpm ctx)
+        get-tr-x (effects/make-param-resolver :tr-x params time-ms bpm ctx)
+        get-tr-y (effects/make-param-resolver :tr-y params time-ms bpm ctx)
+        get-bl-x (effects/make-param-resolver :bl-x params time-ms bpm ctx)
+        get-bl-y (effects/make-param-resolver :bl-y params time-ms bpm ctx)
+        get-br-x (effects/make-param-resolver :br-x params time-ms bpm ctx)
+        get-br-y (effects/make-param-resolver :br-y params time-ms bpm ctx)]
+    (map-indexed
+     (fn [idx pt]
+       (let [x (double (pt t/X))
+             y (double (pt t/Y))
+             tl-x (double (get-tl-x x y idx)) tl-y (double (get-tl-y x y idx))
+             tr-x (double (get-tr-x x y idx)) tr-y (double (get-tr-y x y idx))
+             bl-x (double (get-bl-x x y idx)) bl-y (double (get-bl-y x y idx))
+             br-x (double (get-br-x x y idx)) br-y (double (get-br-y x y idx))
+             ;; Convert from [-1,1] to [0,1] for interpolation
+             u (/ (+ x 1.0) 2.0)  ; 0 at left, 1 at right
+             v (/ (+ y 1.0) 2.0)  ; 0 at bottom, 1 at top
+             ;; Bilinear interpolation
+             ;; P = (1-u)(1-v)*BL + u*(1-v)*BR + (1-u)*v*TL + u*v*TR
+             one-minus-u (- 1.0 u)
+             one-minus-v (- 1.0 v)
+             new-x (+ (* one-minus-u one-minus-v bl-x)
+                      (* u one-minus-v br-x)
+                      (* one-minus-u v tl-x)
+                      (* u v tr-x))
+             new-y (+ (* one-minus-u one-minus-v bl-y)
+                      (* u one-minus-v br-y)
+                      (* one-minus-u v tl-y)
+                      (* u v tr-y))]
+         (t/update-point-xy pt new-x new-y))))))
 
 (effects/register-effect!
  {:id :corner-pin
@@ -271,15 +285,17 @@
 
 
 (defn- lens-distortion-xf [time-ms bpm params ctx]
-  (let [resolved (effects/resolve-params-global params time-ms bpm ctx)
-        k1 (double (:k1 resolved))
-        k2 (double (:k2 resolved))]
-    (map (fn [pt]
-           (let [x (double (pt t/X))
-                 y (double (pt t/Y))
-                 r-sq (+ (* x x) (* y y))
-                 factor (+ 1.0 (* k1 r-sq) (* k2 r-sq r-sq))]
-             (t/update-point-xy pt (* x factor) (* y factor)))))))
+  (let [get-k1 (effects/make-param-resolver :k1 params time-ms bpm ctx)
+        get-k2 (effects/make-param-resolver :k2 params time-ms bpm ctx)]
+    (map-indexed
+     (fn [idx pt]
+       (let [x (double (pt t/X))
+             y (double (pt t/Y))
+             k1 (double (get-k1 x y idx))
+             k2 (double (get-k2 x y idx))
+             r-sq (+ (* x x) (* y y))
+             factor (+ 1.0 (* k1 r-sq) (* k2 r-sq r-sq))]
+         (t/update-point-xy pt (* x factor) (* y factor)))))))
 
 (effects/register-effect!
  {:id :lens-distortion
@@ -311,19 +327,24 @@
 
 
 (defn- wave-distort-xf [time-ms bpm params ctx]
-  (let [resolved (effects/resolve-params-global params time-ms bpm ctx)
-        amplitude (double (:amplitude resolved))
-        frequency (double (:frequency resolved))
-        axis (:axis resolved)
-        speed (double (:speed resolved))
-        time-offset (* (/ (double time-ms) 1000.0) speed)]
-    (map (fn [pt]
-           (let [x (double (pt t/X))
-                 y (double (pt t/Y))]
-             (case axis
-               :x (t/update-point-xy pt x (+ y (* amplitude (Math/sin (* 2.0 Math/PI (+ (* x frequency) time-offset))))))
-               :y (t/update-point-xy pt (+ x (* amplitude (Math/sin (* 2.0 Math/PI (+ (* y frequency) time-offset))))) y)
-               pt))))))
+  (let [get-amplitude (effects/make-param-resolver :amplitude params time-ms bpm ctx)
+        get-frequency (effects/make-param-resolver :frequency params time-ms bpm ctx)
+        get-speed (effects/make-param-resolver :speed params time-ms bpm ctx)
+        ;; axis is typically static, but support modulation for completeness
+        resolved-axis (effects/resolve-params-global {:axis (:axis params)} time-ms bpm ctx)
+        axis (:axis resolved-axis)]
+    (map-indexed
+     (fn [idx pt]
+       (let [x (double (pt t/X))
+             y (double (pt t/Y))
+             amplitude (double (get-amplitude x y idx))
+             frequency (double (get-frequency x y idx))
+             speed (double (get-speed x y idx))
+             time-offset (* (/ (double time-ms) 1000.0) speed)]
+         (case axis
+           :x (t/update-point-xy pt x (+ y (* amplitude (Math/sin (* 2.0 Math/PI (+ (* x frequency) time-offset))))))
+           :y (t/update-point-xy pt (+ x (* amplitude (Math/sin (* 2.0 Math/PI (+ (* y frequency) time-offset))))) y)
+           pt))))))
 
 (effects/register-effect!
  {:id :wave-distort
