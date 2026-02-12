12a8de3cfa2b35be4ed92d61f79f8130f2c78146
Zubair Ahmed <zahmed0808@gmail.com>
2026-02-12 14:10:45 -0800

    commented wrong thing

--- src/laser_show/backend/streaming_engine.clj
+++ src/laser_show/backend/streaming_engine.clj
@@ -75,7 +75,7 @@
                                        service-id 0
                                        output-config output-config/default-config
                                        target-mtu DEFAULT_TARGET_MTU
-                                       fragmentation-enabled? true #_false}}]
+                                       fragmentation-enabled? false #_true}}]
   {:target-host target-host
    :target-port port
    :frame-provider frame-provider
